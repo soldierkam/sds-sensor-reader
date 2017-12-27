b656c415d47a373a4d6a0c2adfd04055ff426656
soldierkam <soldier.kam@gmail.com>
2017-12-27 22:34:56 +0100

    Add configuration commands

--- sds-reader.py
+++ sds-reader.py
@@ -19,37 +19,89 @@ USBPORT  = "/dev/ttyUSB0"
 class SDS011Reader:
 
     def __init__(self, inport):
+        self.id = 0xFFFF
         self.serial = serial.Serial(port=inport,baudrate=9600)
+        #self.readQueryMode()
+        self.setInterval(5)
+        self.setModeActive(True)
+        
+    def readQueryMode(self):
+        packet = self.prepareCmd(0x2, 0x0)
+        self.writePacket(packet)
+        
+    def setModeActive(self, active):
+        packet = self.prepareCmd(0x2, 0x1)
+        packet[4] = 0x00 if active else 0x01
+        self.writePacket(packet)
+        
+    def setDeviceId(self, id):
+        packet = self.prepareCmd(0x5, 0x0)
+        packet[13] = id >> 8
+        packet[14] = id & 0xFF
+        self.writePacket(packet)    
+    
+    def setInterval(self, minutes):
+        if minutes > 30:
+            raise ValueError("Interval should be < 30")
+        packet = self.prepareCmd(0x8, 0x1)
+        packet[4] = minutes
+        self.writePacket(packet)
+        
+    def prepareCmd(self, action, set_op):
+        bytes = [0] * 19
+        bytes[0] = 0xaa
+        bytes[1] = 0xb4
+        bytes[2] = action & 0xFF
+        bytes[3] = set_op & 0xFF
+        bytes[15] = self.id >> 8
+        bytes[16] = self.id & 0xFF
+        bytes[18] = 0xab
+        return bytes
+    
+    def packetToStr(self, packet):
+        return ":".join("{:02x}".format(c) for c in packet)
+    
+    def writePacket(self, packet):
+        chksum = 0
+        for v in packet[2:17]:
+            chksum += v
+            packet[17] = chksum & 0xFF
+        to_send = ''.join(chr(x) for x in packet)
+        print "writing " + self.packetToStr(packet)
+        self.serial.write(to_send)
+
+    def readSensorPacket(self):
+        pck_len = 10
+        packet = [0] * pck_len
+        read = 0
+        while read < pck_len:
+            #toread = self.serial.inWaiting()
+            #if toread == 0:
+            #    time.sleep(0.01)
+            #    continue
+            v = ord(self.serial.read())
+            print "{:02x}".format(v)
+            packet[read] = v
+            read += 1
+            if read == 1 and packet[0] != 0xAA:
+                packet = [0] * pck_len
+                read = 0
+            if read == 2 and packet[1] not in (0xC0, 0xC5):
+                packet = [0] * pck_len
+                read = 0
+        return packet                
 
     def readValue( self ):
-        step = 0
-        while True: 
-            while self.serial.inWaiting()!=0:
-                v=ord(self.serial.read())
-
-                if step ==0:
-                    if v==170:
-                        step=1
-
-                elif step==1:
-                    if v==192:
-                        values = [0,0,0,0,0,0,0]
-                        step=2
-                    else:
-                        step=0
-
-                elif step>8:
-                    step =0
-                    pm25 = values[0]+values[1]*256
-                    pm10 = values[2]+values[3]*256
-                    return [pm25,pm10]
-
-                elif step>=2:
-                    values[step-2]=v
-                    step= step+1
-
-
-
+        while True:
+            packet = self.readSensorPacket()
+            print "received " + self.packetToStr(packet)
+            if packet[1] != 0xC0:
+                print "Wrong type: tring again"
+                continue
+            pm25 = (packet[2] + 256 * packet[3]) / 10
+            pm10 = (packet[4] + 256 * packet[5]) / 10
+            return [pm25, pm10]
+        
     def read( self, duration ):
         start = os.times()[4]
 
@@ -57,7 +109,7 @@ class SDS011Reader:
         species = [[],[]]
         speciesType = ["pm2.5-mg","pm10-mg"]
 
-        while os.times()[4]<start+duration:
+        while os.times()[4] < start+duration:
             try:
                 values = self.readValue()
                 species[0].append(values[0])
@@ -193,7 +245,11 @@ def loop(usbport):
     reader = SDS011Reader(usbport) 
     uploader = SensorDataUploader(SENSORID) 
     while 1:
-        uploader.postValues(reader.read(60))
+	print "Reading.."
+	values = reader.readValue()
+	print "Values: " + str(values)
+	#print "Posting.."
+        #uploader.postValues(values)
 
 
 if len(sys.argv)==2:
